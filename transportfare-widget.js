485600c6882a3aa71be5e33e3ba3a6ddf838d6c2
Thabo <shaunthabo38@gmail.com>
2023-07-13 18:08:09 +0200

    tuesday commit

--- transportfare-widget.js
+++ transportfare-widget.js
@@ -1,16 +1,12 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data ("transportfareWidget", function() {
-     return   { 
-        messagefare: '',
-        fare: '',
-        showGreeting : false,
-        fare1(){
-        this.messagefare = transportFee(this.fare);
-    
-        setTimeout(() => {
-            this.messagefare = '';
-        }, 3000);
-    }
-    }
-    }  );
+    Alpine.data('transportFeeWidget', function () {
+        return {
+            workingShift: '',
+            transportFeeMessage: '',
+            calculate() {
+                const message = transportFee(this.workingShift);
+                this.transportFeeMessage = message;
+            }
+        }
+    });
 })
